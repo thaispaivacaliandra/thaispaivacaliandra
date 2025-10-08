e9ff164d32e0d7e93cece8f6610e10a22a5c806a
thaispaiva <thaispaivacaliandra@gmail.com>
2025-10-08 03:51:02 -0300

    kk

--- types/spline.d.ts
+++ types/spline.d.ts
@@ -1,6 +1,6 @@
 declare namespace JSX {
     interface IntrinsicElements {
-    'spline-viewer': React.DetailedHTMLProps
+    'spline-viewer': React.DetailedHTMLProps<
         React.HTMLAttributes<HTMLElement> & {
         url?: string;
         'events-target'?: string;
