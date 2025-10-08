92f46e11b709f61592702540f492f1021758faab
thaispaiva <thaispaivacaliandra@gmail.com>
2025-10-08 03:51:28 -0300

    kkkk

--- app/3d/page.tsx
+++ app/3d/page.tsx
@@ -6,6 +6,9 @@ declare global {
       'spline-viewer': React.DetailedHTMLProps<
         React.HTMLAttributes<HTMLElement> & {
           url?: string;
+          'events-target'?: string;
+          'loading-anim'?: boolean;
+          'loading-anim-type'?: string;
         },
         HTMLElement
       >;
