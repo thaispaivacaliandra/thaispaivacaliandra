0df35c2da6c7bb3c412f33115e714e1519296c49
thaispaiva <thaispaivacaliandra@gmail.com>
2025-10-08 03:42:03 -0300

    Add spline-viewer type declaration

---
+++ types/spline.d.ts
@@ -0,0 +1,13 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+    'spline-viewer': React.DetailedHTMLProps
+        React.HTMLAttributes<HTMLElement> & {
+        url?: string;
+        'events-target'?: string;
+        'loading-anim'?: boolean;
+        'loading-anim-type'?: string;
+        },
+        HTMLElement
+    >;
+    }
+}
